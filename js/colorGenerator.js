f5683261eef26d1ed6f8a73990d254c0c4eb3de1
Martin Törnros <martin.tornros@ri.se>
2025-03-24 12:26:45 +0100

    Simplified JS

--- js/colorGenerator.js
+++ js/colorGenerator.js
@@ -1,49 +1,46 @@
-let data = {
-    colorAmount: 3,
-    colors: [],
+let colors = []
 
-    // Töm hela färgpaletten
-    clearPalette() {
-        this.colors = []
-    },
+// Töm hela färgpaletten
+function clearPalette() {
+    colors = []
+}
 
-    // Slumpa den första färgen i färgpaletten
-    randomBaseColor() {
-        // Töm färgpaletten
-        this.clearPalette()
+// Slumpa den första färgen i färgpaletten
+function randomBaseColor() {
+    // Töm färgpaletten
+    clearPalette()
 
-        // Slumpa första färgen
-        this.colors[0] = chroma.random()
-    },
+    // Slumpa första färgen
+    colors[0] = chroma.random()
+}
 
-    // Rotera en färg 'color' (färgrymd: HSL) 'degrees' antal grader. Returnera den nya färgen.
-    rotateHue(color, degrees) {
-        return chroma(color).set('hsl.h', '+' + degrees)
-    },
+// Rotera en färg 'color' (färgrymd: HSL) 'degrees' antal grader. Returnera den nya färgen.
+function rotateHue(color, degrees) {
+    return chroma(color).set('hsl.h', '+' + degrees)
+}
 
-    logPalette() {
-        console.log("Färgpalett:", this.colors.toString())
-    },
+function logPalette() {
+    console.log("Färgpalett:", colors.toString())
+}
 
-    // Skapa en färgpalett med tre färger: RGB
-    setPaletteRGB() {
-        this.clearPalette()
-        this.colors[0] = '#ff0000'
-        this.colors[1] = '#00ff00'
-        this.colors[2] = '#0000ff'
+// Skapa en färgpalett med tre färger: RGB
+function setPaletteRGB() {
+    clearPalette()
+    colors[0] = '#ff0000'
+    colors[1] = '#00ff00'
+    colors[2] = '#0000ff'
 
-        // Logga färgpaletten i konsollen
-        this.logPalette()
-    },
+    // Logga färgpaletten i konsollen
+    logPalette()
+}
 
-    // Skapa en färgpalett med tre färger (triadic), baserat på den första färgen. 
-    setPaletteTriadic() {
-        // Varje färg ska roteras 360/3 = 120 grader
-        let step = 360 / 3
-        this.colors[1] = this.rotateHue(this.colors[0], step)
-        this.colors[2] = this.rotateHue(this.colors[0], step * 2)
+// Skapa en färgpalett med tre färger (triadic), baserat på den första färgen. 
+function setPaletteTriadic() {
+    // Varje färg ska roteras 360/3 = 120 grader
+    let step = 360 / 3
+    colors[1] = rotateHue(colors[0], step)
+    colors[2] = rotateHue(colors[0], step * 2)
 
-        // Logga färgpaletten i konsollen
-        this.logPalette()
-    }
+    // Logga färgpaletten i konsollen
+    logPalette()
 }
